765366334f2eb4ea3b38a494d9ff0ea825abf0c0
Sertan Senturk <contact@sertansenturk.com>
2015-11-03 02:27:01 +0100

    Added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='ahenk-identifier',
+      version='0.1',
+      description='Identifies the ahenk (transposition) of a makam music recording given the tonic frequency and the symbol (or the makam)',
+      author='Sertan Senturk',
+      url='http://sertansenturk.com',
+      packages=['ahenkidentifier'],
+)

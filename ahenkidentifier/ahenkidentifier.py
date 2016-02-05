44a1ec4feff73083adf1d00aa3a683c705b805f8
Sertan Senturk <contact@sertansenturk.com>
2016-02-05 22:43:32 +0100

    Closes #1

--- ahenkidentifier/ahenkidentifier.py
+++ ahenkidentifier/ahenkidentifier.py
@@ -4,18 +4,19 @@ import numpy as np
 
 CENTS_IN_OCTAVE = 1200  # cents
 
+
 def identify(tonic_freq, symbol_in):
     tonic_dict = get_tonic_dict()
     ahenks = get_ahenk_dict()
 
     # get the tonic symbol and frequency
     if symbol_in in tonic_dict.keys():  # tonic symbol given
-        tonic_symbol = symbol_in
+        # tonic_symbol = symbol_in
         tonic_bolahenk_freq = tonic_dict[symbol_in]['bolahenk_freq']
     else:  # check if the makam name is given
         for sym, val in tonic_dict.iteritems():
             if symbol_in in val['makams']:
-                tonic_symbol = sym
+                # tonic_symbol = sym
                 tonic_bolahenk_freq = val['bolahenk_freq']
                 break
         if not tonic_bolahenk_freq:
@@ -26,15 +27,22 @@ def identify(tonic_freq, symbol_in):
     cent_dist = hz_to_cent(tonic_freq, tonic_bolahenk_freq)
     mod_cent_dist = np.mod(cent_dist, CENTS_IN_OCTAVE)
 
-    # if the distance is more than 1150 cents wrap it to minus so it will be mapped to 0 cents
-    mod_cent_dist = mod_cent_dist if mod_cent_dist < 1150 else mod_cent_dist-1200
+    # if the distance is more than 1150 cents wrap it to minus
+    # so it will be mapped to 0 cents
+    mod_cent_dist = (mod_cent_dist if mod_cent_dist < 1150
+                     else mod_cent_dist-1200)
 
     mod_cent_approx = int(round(mod_cent_dist * 0.01) * 100)
     mod_cent_dev = abs(mod_cent_approx-mod_cent_dist)
 
-    dist_dict = {'distance_to_bolahenk':{'performed':{'value':mod_cent_dist.tolist()[0], 'unit':'cent'},
-                                         'theoretical':{'value':mod_cent_approx, 'unit':'cent'}},
-                 'deviation':{'value':mod_cent_dev.tolist()[0], 'unit':'cent'}}
+    # create the stats dictionary
+    distance_to_bolahenk = {'performed': {'value': mod_cent_dist.tolist()[0],
+                                          'unit': 'cent'},
+                            'theoretical': {'value': mod_cent_approx,
+                                            'unit': 'cent'}}
+    dist_dict = {'distance_to_bolahenk': distance_to_bolahenk,
+                 'deviation': {'value': mod_cent_dev.tolist()[0],
+                               'unit': 'cent'}}
 
     # get the ahenk
     for ahenk_slug, val in ahenks.iteritems():
@@ -55,12 +63,12 @@ def get_ahenk_dict():
 
 
 def hz_to_cent(hz_track, ref_freq):
-    """-------------------------------------------------------------------------
+    """------------------------------------------------------------------------
     Converts an array of Hertz values into cents.
-    ----------------------------------------------------------------------------
+    ---------------------------------------------------------------------------
     hz_track : The 1-D array of Hertz values
     ref_freq	: Reference frequency for cent conversion
-    -------------------------------------------------------------------------"""
+    ------------------------------------------------------------------------"""
     if type(hz_track) == list:
         hz_track = np.array(hz_track)
     else:
@@ -69,4 +77,3 @@ def hz_to_cent(hz_track, ref_freq):
     # The 0 Hz values are removed, not only because they are meaningless,
     # but also logarithm of 0 is problematic.
     return np.log2(hz_track[hz_track > 0] / ref_freq) * 1200.0
-

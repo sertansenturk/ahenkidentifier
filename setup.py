4c6d0ac597ae1207a368ea6e236a2180f57de632
Sertan Senturk <contact@sertansenturk.com>
2016-03-16 01:34:08 +0100

    Clarified requirements

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@
 from setuptools import setup
 
 setup(name='ahenk-identifier',
-      version='0.1',
-      author='Sertan Senturk',
-      author_email='contact AT sertansenturk DOT com',
-      license='agpl 3.0',
-      description='Identifies the ahenk (transposition) of a makam music recording given the tonic frequency and the symbol (or the makam)',
-      url='http://sertansenturk.com',
-      packages=['ahenkidentifier'],
+    version='0.1',
+    author='Sertan Senturk',
+    author_email='contact AT sertansenturk DOT com',
+    license='agpl 3.0',
+    description='Identifies the ahenk (transposition) of a makam music recording given the tonic frequency and the symbol (or the makam)',
+    url='http://sertansenturk.com',
+    packages=['ahenkidentifier'],
+    install_requires=[
+        "numpy",
+        "future",
+    ],
 )
-

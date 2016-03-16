26e08cf16b688608439ae9229ad24ff40cdaa4bb
Sertan Senturk <contact@sertansenturk.com>
2016-03-16 12:44:24 +0100

    Added missing MANIFEST file for the package data during installation

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(name='ahenkidentifier',
     description='Identifies the ahenk (transposition) of a makam music recording given the tonic frequency and the symbol (or the makam)',
     url='http://sertansenturk.com',
     packages=['ahenkidentifier'],
+    include_package_data=True,
     install_requires=[
         "numpy",
         "future",
